f4c2d69c97a65407f44703ade4cace4865874056
Kocuya <ssmkokuya2525@gmail.com>
2024-02-15 20:12:31 +0900

    a little bit fixes

--- lib/scr/views/pages/details_of_item.dart
+++ lib/scr/views/pages/details_of_item.dart
@@ -1,19 +1,12 @@
 import 'package:flutter/material.dart';
-import '/scr/views/pages/item_editing.dart';
+import 'item_editing.dart';
 
-class DetailsItemPage extends StatefulWidget {
+class DetailsItemPage extends StatelessWidget {
+
   const DetailsItemPage({super.key});
 
   @override
-  DetailsItemState createState() => DetailsItemState();
-}
-
-class DetailsItemState extends State<DetailsItemPage> {
-  String? _frequency = '一週間'; // 通知頻度の初期値
-  final List<String> _frequencies = ['一週間', '一ヶ月', '三ヶ月', '六ヶ月', '一年']; // 通知頻度の選択肢
-
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
         title:const Text('予定詳細'),
@@ -21,10 +14,8 @@ class DetailsItemState extends State<DetailsItemPage> {
         leading: IconButton(
           icon:const Icon(Icons.close),
           onPressed: () {
-            Navigator.pop(
-              context,
-            );
-          },
+            Navigator.pop(context,);
+          }
         ),
         actions: <Widget>[
           IconButton(
@@ -34,52 +25,18 @@ class DetailsItemState extends State<DetailsItemPage> {
                 context,
                 MaterialPageRoute(builder: (context) => const ItemEditingPage())
               );
-            },
-          ),
+            }
+          )
         ],
       ),
-      body: SingleChildScrollView(
-        padding:const EdgeInsets.all(16.0),
+      body:const SingleChildScrollView(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            TextFormField(
-              decoration:const InputDecoration(labelText: '名前', hintText: '名前を入力してください'),
-            ),
-            DropdownButtonFormField(
-              value: _frequency,
-              decoration:const InputDecoration(labelText: '通知頻度'),
-              items: _frequencies.map((String value) {
-                return DropdownMenuItem(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
-              onChanged: (String? newValue) {
-                setState(() {
-                  _frequency = newValue;
-                });
-              },
-            ),
-            TextFormField(
-              decoration:const InputDecoration(labelText: '電話番号', hintText: '電話番号を入力してください'),
-            ),
-            TextFormField(
-              decoration:const InputDecoration(labelText: 'メールアドレス', hintText: 'メールアドレスを入力してください'),
-            ),
-            TextFormField(
-              decoration:const InputDecoration(labelText: '会社名', hintText: '会社名を入力してください'),
-            ),
-            TextFormField(
-              decoration:const InputDecoration(labelText: '役職', hintText: '役職を入力してください'),
-            ),
-            TextFormField(
-              decoration:const InputDecoration(labelText: 'メモ', hintText: 'メモを入力してください'),
-              maxLines: 3, // メモ入力欄を複数行に
-            ),
+            Text('テストテキスト')
           ],
-        ),
-      ),
+        )
+      )
     );
   }
 }
